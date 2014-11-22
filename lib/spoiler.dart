55cef29f06ff5435ecde617f4430f2164291e029
Vitaliy <rasart.pro@gmail.com>
2014-11-23 02:33:03 +0300

    changed spoiler name class

--- lib/spoiler.dart
+++ lib/spoiler.dart
@@ -10,21 +10,35 @@ class CoolSpoilerTag{
 	}
 }
 
-//Create new tag
-class CoolSpoilerTag extends DivElement {
+//For new spoiler tag
+class SpoilerTag extends DivElement {
 	Element tag = new DivElement();
 
-	factory CoolSpoilerTag(){
+	String name = 'Open/Close';
+	String type = 'close';
+
+	factory SpoilerTag(){
 		return tag;
 	}
 
-	CoolSpoiler.created() : super.created(){
-		tag.setAttribute('name','Open/Close');
+	SpoilerTag.created() : super.created(){
+		tag.setAttribute('name','$name');
+		tag.setAttribute('type','$type');
 	}
+}
+
+//For new spoiler name tag
+class SpoilerNameTag extends SpoilerTag {
+
+	factory SpoilerNameTag(){
+		return new DivElement();
+	}
+
+	SpoilerNameTag.created() : super.created(){}
 }
 
 //Register new HTML tags in DOM
 Function RegisterTags(){
-	document.registerElement('cool-spoiler', CoolSpoilerTag, extendsTag: 'div');
-	document.registerElement('spoiler-name', CoolSpoilerTag, extendsTag: 'div');
+	document.registerElement('cool-spoiler', SpoilerTag, extendsTag: 'div');
+	document.registerElement('spoiler-name', SpoilerNameTag, extendsTag: 'div');
 }
